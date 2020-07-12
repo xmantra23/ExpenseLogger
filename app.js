893e3a444811663acf19971582792dfa3a93416b
Samir Shrestha <samir.shrestha92@gmail.com>
2020-07-12 11:49:46 -0500

    basic application working

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var expenseRoutes = require("./routes/expenses");
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/upload_receipts"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
@@ -42,9 +43,10 @@ app.use(function(req,res,next){
 	res.locals.currentUser = req.user;
 	var today = new Date();
 	var month = today.getMonth()+1;
-	month = month < 10 ? '0' + month : '' + month;
-	res.locals.today = today.getFullYear()+'-'+ month +'-'+today.getDate();  //todays date in yyyy-mm-dd format
-	
+	var day = today.getDate();
+	month = month < 10 ? '0' + month : '' + month;  //if month is less than 10 add a 0 prefix . eg 8 -> becomes 08
+	day = day < 10 ? '0' + day: '' + day;
+	res.locals.today = today.getFullYear()+'-'+ month +'-'+day;  //todays date in yyyy-mm-dd format
 	res.locals.formattedDate = function(date){
 		//was getting one day off before doing this.
 		var date = new Date(date.getTime() + date.getTimezoneOffset() * 60000); //converting utc date zone to local date.
@@ -73,8 +75,10 @@ app.use(function(req,res,next){
 	res.locals.format = function(d){
 		var date = new Date(d.getTime() + d.getTimezoneOffset() * 60000); 
 		var month = date.getMonth()+1;
+		var day = date.getDate();
 		month = month < 10 ? '0' + month : '' + month;
-		return date.getFullYear()+'-'+ month +'-'+date.getDate();  //todays date in yyyy-mm-dd format
+		day = day < 10 ? '0' + day: '' + day;
+		return date.getFullYear()+'-'+ month +'-'+day;  //todays date in yyyy-mm-dd format
 	}
 	next();
 });
--- routes/expenses.js
+++ routes/expenses.js
@@ -2,6 +2,44 @@ var express = require("express");
 var router = express.Router();
 var Expense = require("../models/expense");
 
+// file upload settings ------------------------------------------
+var path = require('path');
+var multer = require('multer');
+var fs = require('fs');
+
+var storage = multer.diskStorage({
+  // destination: function (req, file, cb) {
+  // var dir =  'upload_receipts/' + req.user.username;
+  //     fs.exists(dir, exist =>{
+  // if(!exist){
+  // return fs.mkdir(dir, {recursive: true}, err => cb(err,dir))
+  // }
+  // return cb(null, dir);
+  // });
+  // },
+  filename: function (req, file, cb) {
+    cb(null,Date.now() + '-' + Math.round(Math.random() * 1E9)+ '-' + file.originalname);
+  }
+});
+
+var fileFilter = function(req,file,callback){
+ 	var ext = path.extname(file.originalname);
+	if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg' && ext !== '.pdf') {
+		return callback(new Error('Only images and pdfs are allowed'))
+	}
+	callback(null, true)
+}
+
+var upload = multer({storage:storage,fileFilter:fileFilter});
+var cloudinary = require('cloudinary');
+cloudinary.config({
+	cloud_name: 'xmantra23',
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
+});
+//-----------------------------------------------------------------------------------------------
+
+//new
 router.get("/expenses/new",function(req,res){
 	if(req.isAuthenticated()){
 		res.render("expenses/new");
@@ -10,6 +48,7 @@ router.get("/expenses/new",function(req,res){
 	}
 });
 
+//edit
 router.get("/expenses/:id/edit",function(req,res){
 	if(req.isAuthenticated()){
 		Expense.findById(req.params.id,function(err,foundExpense){
@@ -24,24 +63,88 @@ router.get("/expenses/:id/edit",function(req,res){
 	}
 });
 
-router.post("/expenses",function(req,res){
+//create
+router.post("/expenses",upload.single('receipt'),function(req,res){
 	if(req.isAuthenticated()){
-		var newExpense = new Expense(req.body.expense);
-		newExpense.creator.id = req.user._id;
-		newExpense.creator.username = req.user.username;
-		Expense.create(newExpense,function(err,newlyCreatedExpense){
+		if(req.fileValidationError){
+			return res.redirect("/expenses/new");
+		}
+		//console.log(req.file);
+		cloudinary.v2.uploader.upload(req.file.path,
+		{folder:"ExpenseTracker/Upload_Receipts/" + req.user.username},function(err,result){
 			if(err){
-				console.log(err.message);
-				return res.redirect("/expenses");
-			}else{
-				res.redirect("/expenses");
+				console.log(err);
+				return res.redirect('back');
 			}
+			var newExpense = new Expense(req.body.expense);
+			newExpense.creator.id = req.user._id;
+			newExpense.creator.username = req.user.username;
+			newExpense.receiptImage = result.secure_url;
+			newExpense.receiptId = result.public_id;
+			Expense.create(newExpense,function(err,newlyCreatedExpense){
+				if(err){
+					console.log(err.message);
+					return res.redirect("/expenses");
+				}else{
+					res.redirect("/expenses");
+				}
+			});
 		});
 	}else{
 		res.send("Not logged in");
 	}
 });
 
+//update
+router.put("/expenses/:id",upload.single('receipt'),function(req,res){
+	if(req.isAuthenticated()){
+		Expense.findById(req.params.id, async function(err,foundExpense){
+			if(err){
+				console.log(err);
+			}else{
+				if(req.file){
+					if(req.fileValidationError){
+						return res.redirect("/expenses");
+					}
+					try{
+						await cloudinary.v2.uploader.destroy(foundExpense.receiptId);
+						var result = await cloudinary.v2.uploader.upload(req.file.path,
+								{folder:"ExpenseTracker/Upload_Receipts/" + req.user.username});
+						foundExpense.receiptImage = result.secure_url;
+						foundExpense.receiptId = result.public_id;
+					}catch(err){
+						console.log(err);
+						return res.redirect("back");
+					}
+				}
+			}
+			
+			foundExpense.title = req.body.title;
+			foundExpense.total = req.body.total;
+			foundExpense.location = req.body.location;
+			foundExpense.memo = req.body.memo;
+			foundExpense.transactionDate = req.body.transactionDate;
+			foundExpense.updatedAt = Date.now();
+			foundExpense.save();
+			res.redirect("/expenses");
+		});
+	};
+
+});
+
+//show
+router.get("/receipts/:id",function(req,res){
+	Expense.findById(req.params.id,function(err,foundExpense){
+			if(err){
+				console.log(err.message);
+			}else{
+				var fileType = (foundExpense.receiptImage).split('.').pop();
+				res.render("receipts/show",{file:foundExpense.receiptImage,fileType:fileType});
+			}
+	});	
+});
+
+//index
 router.get("/expenses",function(req,res){
 	if(req.isAuthenticated()){
 		Expense.find({"creator.id":req.user._id},function(err,foundExpenses){
@@ -56,13 +159,23 @@ router.get("/expenses",function(req,res){
 	}
 });
 
+//destroy
 router.delete("/expenses/:id",function(req,res){
 	if(req.isAuthenticated()){
 		Expense.findByIdAndRemove(req.params.id,function(err,removedExpense){
 			if(err)
 				console.log(err);
-			else
-				res.redirect("/expenses");
+			else{
+				try{
+					cloudinary.v2.uploader.destroy(removedExpense.receiptId);
+					res.redirect("/expenses");
+				}catch(err){
+					if(err){
+						console.log("Couldn't delete image /n error: ",err.message );
+						return res.redirect("/expenses");
+					}
+				}
+			}
 		});
 	}else{
 		res.send("Not logged in");
